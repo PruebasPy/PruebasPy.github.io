81be08853f84fc02b3f4d00d1cb46448e0e2098f
PruebasPy <proyectosdeprogramacionpruebas@gmail.com>
2024-06-30 21:56:12 -0500

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,20 @@
+document.getElementById("registroForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    var formData = new FormData(document.getElementById("registroForm"));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://formspree.io/proyectosdeprogramacionpruebas@gmail.com");
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                alert("Registro exitoso. Se ha enviado la información a tu correo electrónico.");
+            } else {
+                alert("Ha ocurrido un error. Inténtalo de nuevo.");
+            }
+        }
+    };
+
+    xhr.send(formData);
+});
